c56b5f350cf600627efd235e8b02ebf7e867adb7
Scott Ian <sim2487@gmail.com>
2018-11-15 14:05:42 -0800

    further work

--- starwars.js
+++ starwars.js
@@ -9,20 +9,37 @@ const searchTerm = process.argv[2];
 
 
 axios.get(`${url}${searchTerm}`)
-  .then(function (res) {
+  .then(function(res) {
+    const gender = res.data.results[0].gender === 'male' ? 'he' : 'she';
     console.log(`${res.data.results[0].name} has been found!
-    ////////////// ${res.data.results[0].name} has also been associated with a total of ${res.data.results[0].vehicles.length} vehicles and ${res.data.results[0].starships.length} starships.`
-    ); 
-    let promises = res.data.results[0].films.map(filmURL => axios.get(filmURL));
-    return Promise.all(promises);
+
+   ${gender} has also been associated with a
+    total of ${res.data.results[0].vehicles.length} vehicles
+     and ${res.data.results[0].starships.length} starships.`
+    );
+
+    //console.log(res.data.results[0]);
+    let films = res.data.results[0].films.map(filmURL => axios.get(filmURL));
+    //console.log(films); why is this pending
+    return Promise.all(films);
   })
   .then(function (res) {
-    res.forEach(result => console.log(result.data));
+
+    const filmArray = res.map(film => ({title: film.data.title, releaseDate: film.data.release_date}));
+    function compare(a, b) {
+      if (a.releaseDate < b.releaseDate)
+        return -1;
+      if (a.releaseDate > b.releaseDate)
+        return 1;
+      return 0;
+    }
+    filmArray.sort(compare);
+    console.log();
   })
   .catch(function (error) {
     console.log( `${searchTerm} is not an entity we're looking for. Move along.`);
   });
-  
-  // .then(function (res) {
-  //   axios.get('https://swapi.co/api/films') 
-  // }
+
+// .then(function (res) {
+//   axios.get('https://swapi.co/api/films')
+// }
