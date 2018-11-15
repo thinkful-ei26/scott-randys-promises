1df15dd769ad9690d3bb8480439a619beff7cdb7
randy sartor <randal.sartor@gmail.com>
2018-11-15 12:46:09 -0500

    figuring out how to sort by release date

--- starwars.js
+++ starwars.js
@@ -1,18 +1,28 @@
 'use strict';
 /* eslint-env jquery*/
+
+
 const axios = require('axios');
-const url = 'https://swapi.co/api/people/1';
+const url = 'https://swapi.co/api/people/?search=';
+const searchTerm = process.argv[2];
 
 
-axios.get(url)
+
+axios.get(`${url}${searchTerm}`)
   .then(function (res) {
-    // handle success
-    console.log(res.data.name);
+    console.log(`${res.data.results[0].name} has been found!
+    ////////////// ${res.data.results[0].name} has also been associated with a total of ${res.data.results[0].vehicles.length} vehicles and ${res.data.results[0].starships.length} starships.`
+    ); 
+    let promises = res.data.results[0].films.map(filmURL => axios.get(filmURL));
+    return Promise.all(promises);
+  })
+  .then(function (res) {
+    res.forEach(result => console.log(result.data));
+  })
+  .catch(function (error) {
+    console.log( `${searchTerm} is not an entity we're looking for. Move along.`);
   });
-  // .catch(function (error) {
-  //   // handle error
-  //   console.log(error);
-  // })
-  // .then(function () {
-  //   // always executed
-   //}
+  
+  // .then(function (res) {
+  //   axios.get('https://swapi.co/api/films') 
+  // }
